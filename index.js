8d60214f2b7afbbf61dc81ac4befab21b28d7b43
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-19 06:13:44 +0700

    chore:initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express =  require('express');
+const app = express();
+const port = 3000;
+
+let songs = [
+  {
+    id: 1,
+    title: 'Twilight',
+    artist: 'Amarynn, Burek',
+    song: '',
+  },
+  {
+    id: 2,
+    title: 'Precious',
+    
+  }
+]
