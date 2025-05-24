d756989a2a3f9b39c9db5ae498336859abd42d9b
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-24 19:09:25 +0700

    feat(song): add routes for get songs

--- src/routes/songs.routes.ts
+++ src/routes/songs.routes.ts
@@ -2,4 +2,6 @@ import { Router } from "express";
 import { getAllSongs } from "../controllers/songs.controller";
 
 const router = Router()
-router.get("")
+router.get("/", getAllSongs)
+
+export default router
