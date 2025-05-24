cd728cdde35781da4c4ffebce8c5006422306be5
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-24 19:14:34 +0700

    feat(song): index settings

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
-const express =  require('express');
-const app = express();
-const port = 3000;
+import router from "./routes/songs.routes";
+import express from "express"
+import path from "path";
 
-let songs = [
-  {
-    id: 1,
-    title: 'Twilight',
-    artist: 'Amarynn, Burek',
-    song: '',
-  },
-  {
-    id: 2,
-    title: 'Precious',
-    
-  }
-]
+const app = express()
+
+app.use(express.json())
+app.use("/music", express.static(path.join(__dirname, "../public/music")))
+app.use("/api", router)
+
+app.listen(3000, () => {
+  console.log("Server is running...")
+})
